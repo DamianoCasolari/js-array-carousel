dbf8b0b036dfb8ddf9899483668d1dd6ee933030
DamianoCasolari <damianocasolari@gmail.com>
2023-03-14 16:49:25 +0100

    add carosel effect with limits

--- assets/jv/main.js
+++ assets/jv/main.js
@@ -1,3 +1,4 @@
+// creo l'array degli src
 const listImgEl = [
     "./assets/img/img/01.webp",
     "./assets/img/img/02.webp",
@@ -9,6 +10,48 @@ const imagesContainerEl = document.querySelector(".images")
 let activeImg = 0;
 
 for (let i = 0; i < listImgEl.length; i++) {
-    imagesContainerEl.innerHTML += `<img class="img-fluid ${i == activeImg ? 'active' : ''}" src="${listImgEl[i]}" alt="imagin_${i}">`
+    // imagesContainerEl.innerHTML += `<img class="img-fluid ${i == activeImg ? 'active' : ''}" src="${listImgEl[i]}" alt="imagin_${i}">`   OTHER OPTION
+    imagesContainerEl.insertAdjacentHTML('beforeend', `<img class="img-fluid ${i == activeImg ? 'active' : ''}" src="${listImgEl[i]}" alt="imagine_${i}">`)
 }
 
+const btnPrevEl = document.querySelector(".prev")
+const btnNextEl = document.querySelector(".next")
+// creo l'array delle <img> caricate nel dom (non degli src)
+const listImagesDom = document.querySelectorAll(".slider .images img")
+
+btnNextEl.addEventListener("click", function () {
+
+
+    const currentImg = listImagesDom[activeImg]
+    currentImg.classList.remove("active");
+    activeImg++;
+    if (activeImg > listImagesDom.length - 1) {
+        activeImg = 0;
+        const newImg = listImagesDom[activeImg]
+        newImg.classList.add("active");
+    } else {
+        const newImg = listImagesDom[activeImg]
+        newImg.classList.add("active");
+    }
+
+    console.log(activeImg)
+
+})
+btnPrevEl.addEventListener("click", function () {
+
+
+    const currentImg = listImagesDom[activeImg]
+    currentImg.classList.remove("active");
+    activeImg--;
+    if (activeImg < 0) {
+        activeImg = 4;
+        const newImg = listImagesDom[activeImg]
+        newImg.classList.add("active");
+    } else {
+        const newImg = listImagesDom[activeImg]
+        newImg.classList.add("active");
+    }
+
+    console.log(activeImg)
+
+})
